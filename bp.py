a18246de84f2835189414a27b5c4c3b58e2aa4a3
mcgrady164 <zhangjun@zhangjundeMacBook-Pro.local>
2015-07-15 10:39:09 -0700

    add german.dat into program

--- bp.py
+++ bp.py
@@ -4,24 +4,36 @@ from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.structure import TanhLayer
 from pybrain.structure import SigmoidLayer
 
-net = buildNetwork(2,10,1,bias=True,hiddenclass=SigmoidLayer)
+net = buildNetwork(20,5,1,bias=True,hiddenclass=SigmoidLayer,outclass=SigmoidLayer)
 
-ds = SupervisedDataSet(2,1)
+ds = SupervisedDataSet(20,1)
 
-ds.addSample([0.5,0.5],[1])
-ds.addSample([0.2,0.5],[1])
-ds.addSample([-0.3,-0.3],[-1])
-ds.addSample([-0.1,-1.0],[-1])
+f = open("german.dat","r")
 
-trainer = BackpropTrainer(net,ds,learningrate=0.1,momentum=0.1,verbose=True)
+for line in open("german.dat"):
+	line = f.readline()
+	data = line.split(",")
+	input = []
+	output = []
+	for i in range(0,len(data)):
+		if i < len(data) - 1:
+			input.append(data[i])
+		else:
+			output.append(data[i])
+	ds.addSample(input,output)
+
+
+
+trainer = BackpropTrainer(net,ds,learningrate=0.5,momentum=0.1,verbose=True)
 
 while 1:
 
-	error = trainer.trainEpochs(epochs=100)
+	error = trainer.trainEpochs(epochs=50)
 
 	if error < 0.01:
 
 		break
 
 
-print net.activate([1.4,1.2])
+print trainer.activate([1,30,2,2,6350,5,5,4,3,1,4,2,31,3,2,1,3,1,1,1])
+
